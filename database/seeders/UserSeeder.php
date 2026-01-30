152e51658214509d7da36ca9030cb513ab90e10b
naqilafadilla <naqilafadilla2@gmail.com>
2026-01-30 16:00:33 +0700

    fix seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
             'name' => 'Administrasi',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin123'),
+            'role' => 'admin',
         ]);
 
         User::create([
